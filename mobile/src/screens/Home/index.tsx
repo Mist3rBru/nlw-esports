cc65a019c8103cb6ed22bc0bbdead73168deff0e
Bruno Rocha <brunorocha2674@gmail.com>
2022-09-15 15:00:28 -0300

    refactor(mobile): add safe area view

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import { View, Image, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Image, FlatList } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-import { Card, Heading } from '../../components'
+import { Card, Game, Heading } from '../../components'
 
 import logoImg from '../../assets/logo-nlw-esports.png'
-import { GAMES } from '../../utils/games'
 import { styles } from './styles'
 
 export function Home() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3030/games')
+      .then(res => res.json())
+      .then(data => setGames(data))
+  }, [])
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
       <Heading
         title="Encontre seu duo"
         subtitle="Seleciona o game que deseja jogar..."
       />
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={game => game.id}
         renderItem={({ item }) => <Card {...item} />}
         showsHorizontalScrollIndicator={false}
         horizontal={true}
         contentContainerStyle={styles.contentList}
       />
-    </View>
+    </SafeAreaView>
   )
 }
