07d0d0fbd85c6f4c82bff063020cbc092b1ac1a2
Bruno Rocha <brunorocha2674@gmail.com>
2022-09-16 13:12:46 -0300

    feat(mobile): aula 4

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Image, FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import {useNavigation } from '@react-navigation/native'
 
 import { Card, Game, Heading } from '../../components'
 
@@ -9,12 +10,18 @@ import { styles } from './styles'
 
 export function HomeScreen() {
   const [games, setGames] = useState<Game[]>([])
+  const { navigate } = useNavigation()
+  
+  const handleOpenGame = (game: Game) => {
+    navigate('game', game)
+  }
 
   useEffect(() => {
     fetch('http://localhost:3030/games')
       .then(res => res.json())
       .then(data => setGames(data))
   }, [])
+
 
   return (
     <SafeAreaView style={styles.container}>
@@ -26,7 +33,7 @@ export function HomeScreen() {
       <FlatList
         data={games}
         keyExtractor={game => game.id}
-        renderItem={({ item }) => <Card {...item} />}
+        renderItem={({ item }) => <Card {...item} onPress={() => handleOpenGame(item)}/>}
         showsHorizontalScrollIndicator={false}
         horizontal={true}
         contentContainerStyle={styles.contentList}
